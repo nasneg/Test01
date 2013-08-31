a0914009244528b09c0b9a73fa92ab6372c90b27
nasneg <hashizume.masaru@gmail.com>
2013-08-31 13:36:18 +0900

    init

---
+++ src/com/example/test/MainActivity.java
@@ -0,0 +1,44 @@
+
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity {
+
+    private WsParserTask<WsParser> mTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.stop).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mTask.cancel(false);
+                mTask = null;
+            }
+        });
+        findViewById(R.id.run).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mTask = new WsParserTask<WsParser>();
+                mTask.execute(new WsParser());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
---
+++ src/com/example/test/WsParser.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Masaru Hashizume<hashizume.masaru@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.test;
+
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.util.Log;
+
+/**
+ *
+ */
+public class WsParser {
+    @SuppressWarnings("unused")
+    private static final String TAG = "WsParser";
+    private final WsParser self = this;
+
+    public <T extends AsyncTask<?, ?, ?>> int execute(T task) {
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            // キャンセルされたら抜ける
+            if (task.isCancelled()) {
+                return 0;
+            }
+
+            SystemClock.sleep(100);
+            count++;
+            Log.d("hashizume", "count:" + count);
+        }
+        return count;
+    }
+}
